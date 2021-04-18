6839b7af2a7d76bdd3c8100b95a59f69e78e3abc
nbboutique <46777706+nbboutique@users.noreply.github.com>
2021-04-18 18:06:55 +0300

    Add files via upload

---
+++ untitled/cmake-build-debug/CMakeFiles/chat_client.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/chat_client.dir/chat_client.cpp.obj"
+  "chat_client.exe"
+  "chat_client.exe.manifest"
+  "chat_client.lib"
+  "chat_client.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/chat_client.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

9ba3b70af1e4172f4bf043bfa9f772197315ac2b
nbboutique <46777706+nbboutique@users.noreply.github.com>
2021-04-18 19:53:50 +0300

    Add files via upload

---
+++ untitled/cmake-build-debug/CMakeFiles/chat_message.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/chat_message.dir/chat_message.cpp.obj"
+  "chat_message.lib"
+  "chat_message.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/chat_message.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ untitled/cmake-build-debug/CMakeFiles/chat_server.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/chat_server.dir/chat_server.cpp.obj"
+  "chat_server.exe"
+  "chat_server.exe.manifest"
+  "chat_server.lib"
+  "chat_server.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/chat_server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
